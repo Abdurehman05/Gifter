e773c634af9ca67897029b9368327e2f0f7e5a49
Abdu <abdukr5@gmail.com.com>
2021-01-12 12:40:17 -0600

    CRUD for Post from chapter

---
+++ Gifter/Repositories/IPostRepository.cs
@@ -0,0 +1,13 @@
+﻿using Gifter.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Gifter.Repositories
+{
+    public interface IPostRepository
+    {
+        List<Post> GetAll();
+        Post GetById(int id);
+        List<Post> GetPostByUserProfileId(int id);
+    }
+}
